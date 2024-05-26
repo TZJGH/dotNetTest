db792cba7324ed4a0a5dfe0a263f6282d014ded9
tianzhenjie <727117719@qq.com>
2024-05-26 19:24:26 +0800

    add benchmarkDotNet and performances

---
+++ BenchmarkDotNet/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+#if true
+//BenchmarkRunner.Run(typeof(Test));
+BenchmarkSwitcher.FromAssembly(typeof(Test).Assembly).Run(args);
+
+[HideColumns("Error")]
+[DisassemblyDiagnoser]
+public class Test
+{
+    [Benchmark]
+    public int Sum() => Sum(1000);
+
+
+    public static int Sum(int count)
+    {
+        var r = 0;
+        for (int i = 1; i <= count; i++)
+        {
+            r += i;
+        }
+        return r;
+    }
+}
+#endif
+
---
+++ BenchmarkDotNet/ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Tests>();
+
+[MemoryDiagnoser]
+[DisassemblyDiagnoser]
+[ThreadingDiagnoser]
+[JsonExporter]
+public class Tests
+{
+    //private const int N = 10000;
+    //private readonly byte[] data;
+
+    //private readonly SHA256 sha256 = SHA256.Create();
+    //private readonly MD5 md5 = MD5.Create();
+
+    //public Md5VsSha256()
+    //{
+    //    data = new byte[N];
+    //    new Random(42).NextBytes(data);
+    //}
+
+    //[Benchmark]
+    //public byte[] Sha256() => sha256.ComputeHash(data);
+
+    //[Benchmark]
+    //public byte[] Md5() => md5.ComputeHash(data);
+
+
+    [Benchmark]
+    public int Sum()
+    {
+        int r = 0;
+        for (int i = 1; i <= 1000; i++)
+        {
+            r += i;
+        }
+        return r;
+    }
+
+    [Benchmark]
+    public int Sum2()
+    {
+        return (1 + 1000) * 1000 / 2;
+    }
+}
