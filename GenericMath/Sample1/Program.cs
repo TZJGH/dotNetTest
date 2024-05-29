7c7e7bda501ed879aef74386638d7960c62ae2dc
tianzhenjie <727117719@qq.com>
2024-05-29 09:33:47 +0800

    add serialization

---
+++ GenericMath/Sample1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Numerics;
+
+Console.WriteLine(Add(int.Parse(args[0]), int.Parse(args[1])));
+
+static T Add<T>(T left, T right)
+    where T : INumber<T>
+{
+    return left + right;
+}
